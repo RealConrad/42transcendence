25aef83a252ff89db52a3a3450444651d5d73911
cwenz <wenzconrad@gmail.com>
2024-06-17 14:45:22 +0200

    Changed to defualt token refresh/pair views

--- backend/auth_service/authentication/urls.py
+++ backend/auth_service/authentication/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import RegisterView, LoginView, DeleteUserView, CustomTokenRefreshView
+from .views import RegisterView, LoginView, DeleteUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
 	path('register/', RegisterView.as_view(), name='register'),
 	path('login/', LoginView.as_view(), name='login'),
 	path('delete/', DeleteUserView.as_view(), name='delete'),
-	path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+	path('token/pair/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
