0a5a34b53d501618df1c982d4b2986fabc744998
cwenz <wenzconrad@gmail.com>
2024-06-17 14:44:43 +0200

    delete api working. forgot to include package

--- backend/auth_service/auth_service/settings.py
+++ backend/auth_service/auth_service/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'rest_framework',
+	'rest_framework_simplejwt',
 
 	# Custom Defined
 	'authentication',
@@ -56,6 +58,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 OAUTH_SETTINGS = {
 	'CLIENT_ID': env('42_CLIENT_UID'),
