a9acc9c0ccd07a343484d892a4fbf899b01a7c8f
cwenz <wenzconrad@gmail.com>
2024-06-18 13:32:02 +0200

    Store oauth tokens in DB generated from 42 and return appripriate response

--- backend/auth_service/oauth/views.py
+++ backend/auth_service/oauth/views.py
@@ -3,6 +3,10 @@ from django.conf import settings
 import urllib
 from django.utils.crypto import get_random_string
 from django.http import JsonResponse, HttpResponseBadRequest
+from django.contrib.auth.models import User
+from .models import UserProfile, OAuthToken
+from django.utils import timezone
+from rest_framework_simplejwt.tokens import RefreshToken
 import requests
 
 def oauthLogin(request):
@@ -29,8 +33,6 @@ def oauthCallback(request):
 		return HttpResponseBadRequest("No code or state provided for oauth callback")
 
 	session_state = request.session.pop('oauth_state', None)
-	print(f"State in session: {session_state}")
-	print(f"State from callback: {state}")
 
 	if state != session_state:
 		return HttpResponseBadRequest("Login rejected. Invalid state parameter")
@@ -48,5 +50,52 @@ def oauthCallback(request):
 	
 	tokens = response.json()
 	access_token = tokens.get('access_token')
-	refresh_token = tokens.get('refresh_tokens')
-	return JsonResponse(tokens)
+	refresh_token = tokens.get('refresh_token')
+	expires_in = tokens.get('expires_in')
+
+	# Send a request to fetch the user data
+	user_info_response = requests.get('https://api.intra.42.fr/v2/me', headers={'Authorization': f'Bearer {access_token}'})
+	if user_info_response.status_code != 200:
+		return HttpResponseBadRequest("Failed to obtain user information")
+
+	user_info = user_info_response.json()
+	email = user_info.get('email')
+	image_url = user_info['image']['link']
+	display_name = user_info.get('displayname')
+	username = user_info.get('login')
+
+	# Create or update the user in our database
+	user, created = User.objects.get_or_create(email=email, defaults={'username': username})
+	if not created:
+		user.username = username
+		user.save()
+	
+	# Update user's profile
+	user_profile, _ = UserProfile.objects.get_or_create(user=user)
+	user_profile.display_name = display_name
+	user_profile.image_url = image_url
+	user_profile.save()
+
+	# Store OAuth tokens
+	OAuthToken.objects.update_or_create(
+		user=user,
+		defaults={
+			'access_token': access_token,
+			'refresh_token': refresh_token,
+			'expires_in': expires_in,
+			'created_at': timezone.now()
+		}
+	)
+
+	# Generate tokens for user (this will be stored on front end)
+	refresh = RefreshToken.for_user(user)
+	return JsonResponse({
+		'access_token': str(refresh.access_token),
+		'refresh_token': str(refresh),
+		'user': {
+			'username': user.username,
+			'email': user.email,
+			'display_name': user_profile.display_name,
+			'image_url': user_profile.image_url,
+		}
+	})
