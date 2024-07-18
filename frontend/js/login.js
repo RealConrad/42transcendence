517c644385f72d965295a5685ddd3bf59cbe06fb
VIVI <lenardviiven@yahoo.de>
2024-07-18 14:14:44 +0200

    change login font

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,15 +1,29 @@
-var modal = document.getElementById("login-modal");
+var loginModal = document.getElementById("login-modal");
 var loginButton = document.getElementById("login-button");
-var closeButton = document.getElementById("close-button");
+var closeButton = document.getElementsById("close-button");
 
-closeButton.style.backgroundColor = "black";
+closeButton[0].style.backgroundColor = "black";
 
 loginButton.onclick = function() {
 	console.log("clicked loginButton :))");
-	modal.style.display = "block";
-}
+	loginModal.style.display = "block";
+	}
+	
+	closeButton[0].onclick = function() {
+		console.log("clicked close button of login page");
+		loginModal.style.display = "none";
+		}
 
-closeButton.onclick = function() {
-	console.log("clicked close button of login page");
-	modal.style.display = "none";
-}
+
+
+var registerModal = document.getElementById("register-modal");
+var registerLink = document.getElementById("registerButton");
+
+registerLink.addEventListener("click", function (){
+	alert("i am clicked yay");
+	console.log("wtf");
+});
+
+// registerLink.onclick = function(){
+// 	console.log("this registration link was clicked hihi");
+// }
