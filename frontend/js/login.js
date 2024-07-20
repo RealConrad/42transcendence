7974840ece8c09390aed7756b50db2ba92cd58fe
Vivi <lenardvivien@yahoo.de>
2024-07-20 20:28:50 +0200

    frontend: basic login and registration form working

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,29 +1,29 @@
 var loginModal = document.getElementById("login-modal");
 var loginButton = document.getElementById("login-button");
-var closeButton = document.getElementsById("close-button");
-
-closeButton[0].style.backgroundColor = "black";
+var closeButton = document.getElementsByTagName("close-button");
+var ToRegisterButton = document.getElementById("GoToRegister-button");
+var ToSignInButton = document.getElementById("GoToSignIn-button");
+var registerModal = document.getElementById("register-modal");
 
 loginButton.onclick = function() {
 	console.log("clicked loginButton :))");
 	loginModal.style.display = "block";
-	}
-	
-	closeButton[0].onclick = function() {
-		console.log("clicked close button of login page");
-		loginModal.style.display = "none";
-		}
-
-
-
-var registerModal = document.getElementById("register-modal");
-var registerLink = document.getElementById("registerButton");
-
-registerLink.addEventListener("click", function (){
-	alert("i am clicked yay");
-	console.log("wtf");
-});
-
-// registerLink.onclick = function(){
-// 	console.log("this registration link was clicked hihi");
-// }
+}
+closeButton[0].onclick = function() {
+	console.log("Login:  clicked close Button");
+	loginModal.style.display = "none";
+}
+closeButton[1].onclick = function() {
+	console.log("Registration: clicked close Button");
+	registerModal.style.display = "none";
+}
+ToRegisterButton.onclick = function(){
+	console.log("clicked register buttoooon");
+	loginModal.style.display = "none";
+	registerModal.style.display = "block";
+}
+ToSignInButton.onclick = function(){
+	console.log("clicked go to sign in button");
+	registerModal.style.display = "none";
+	loginModal.style.display = "block";
+}
