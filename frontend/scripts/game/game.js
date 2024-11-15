8111e15c9bbaf25583bab96cb772026aa705d022
cwenz <wenzconrad@gmail.com>
2024-11-15 23:20:34 +0100

    Moved majority of game initializaiton into the game constructor

--- frontend/scripts/game/game.js
+++ frontend/scripts/game/game.js
@@ -1,21 +1,43 @@
 import { CANVAS_WIDTH, CANVAS_HEIGHT, MAX_SCORE } from "../utils/constants.js";
+import {PADDLE_HEIGHT, PADDLE_SPEED, PADDLE_WIDTH} from "../utils/constants.js";
+import Paddle from "./models/Paddle.js";
+import Player from "./models/Player.js";
+import Ball from "./models/Ball.js";
+import HumanController from "./controllers/HumanController.js";
+import RenderManager from "./managers/RenderManager.js";
+import CollisionManager from "./managers/CollisionManager.js";
+
 
 export default class Game {
-    constructor(canvas, ctx, player1, player2, playerPaddle, opponentPaddle, ball, playerController, opponentController) {
+    constructor(canvas) {
+        this.isGameOver = false;
+        this.isGamePaused = false;
+        this.winner = null;
+        this.maxScore = MAX_SCORE;
+
+        // Setup canvas
         this.canvas = canvas;
-        this.ctx = ctx;
-        this.player1 = player1;
-        this.player2 = player2;
-        this.playerPaddle = playerPaddle;
-        this.opponentPaddle = opponentPaddle;
-        this.ball = ball;
-        this.playerController = playerController;
-        this.opponentController = opponentController;
-        this.gameState = {
-            isGameOver: false,
-            winner: null,
-            maxScore: MAX_SCORE
-        };
+        this.ctx = canvas.getContext('2d');
+
+        this.ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 5, 5);
+
+        // Setup player models and controllers
+        const playerPaddle = new Paddle(10, canvas.height / 2, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_SPEED)
+        const opponentPaddle = new Paddle(canvas.width - 10, canvas.height / 2, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_SPEED)
+        const playerController = new HumanController(playerPaddle, 'w', 's');
+        const opponentController = new HumanController(opponentPaddle, 'ArrowUp', 'ArrowDown'); // TODO: DETERMINE IF ITS AI OR NOT
+        this.player1 = new Player('Player 1', playerPaddle, playerController);
+        this.player2 = new Player('Player 2', opponentPaddle, opponentController);
+
+        // Setup Managers
+        this.renderManager = new RenderManager(this.ctx);
+        this.collisionManager = new CollisionManager(this);
+
+        // Register renderable objects
+        this.renderManager.addRenderable(this.ball);
+        this.renderManager.addRenderable(this.player1.paddle);
+        this.renderManager.addRenderable(this.player2.paddle);
+        this.renderManager.render();
     }
 
     start() {
@@ -23,80 +45,49 @@ export default class Game {
     }
 
     gameLoop() {
-        if (this.gameState.isGameOver) return;
+        if (this.isGameOver || this.isGamePaused) return;
 
-        this.update();
-        this.render();
+        this.collisionManager.handleCollisions()
+        this.ball.move();
+        this.player1.controller.update();
+        this.player2.controller.update();
+        this.checkScoring();
+        this.checkWinCondition();
+        this.renderManager.render();
         requestAnimationFrame(this.gameLoop.bind(this));
     }
 
-    update() {
-        this.playerController.update();
-        this.opponentController.update();
-        this.ball.move();
-        this.checkCollisions();
-        this.checkScoring();
-        this.checkWinCondition();
-    }
-
-    render() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.playerPaddle.draw(this.ctx);
-        this.opponentPaddle.draw(this.ctx);
-        this.ball.draw(this.ctx);
-        this.updateScoreUI();
-    }
-
-    checkCollisions() {
-        this.checkWallCollision();
-        this.checkPaddleCollision(this.playerPaddle);
-        this.checkPaddleCollision(this.opponentPaddle);
-    }
-
-    checkWallCollision() {
-        if (this.ball.y - this.ball.radius < 0 || this.ball.y + this.ball.radius > CANVAS_HEIGHT) {
-            this.ball.speedY *= -1;
-        }
-    }
-
-    checkPaddleCollision(paddle) {
-        if (
-            this.ball.x - this.ball.radius <= paddle.x + paddle.width &&
-            this.ball.x + this.ball.radius > paddle.x &&
-            this.ball.y - this.ball.radius < paddle.y + paddle.height &&
-            this.ball.y + this.ball.radius > paddle.y
-        ) {
-            this.ball.speedX *= -1;
-        }
+    togglePause() {
+        this.isGamePaused = !this.isGamePaused;
     }
 
     checkScoring() {
         if (this.ball.x - this.ball.radius < 0) {
             this.player2.incrementScore();
             this.resetGameState();
+            this.updateScoreUI();
         }
         else if (this.ball.x + this.ball.radius > CANVAS_WIDTH) {
             this.player1.incrementScore();
             this.resetGameState();
+            this.updateScoreUI();
         }
     }
 
     checkWinCondition() {
-        if (this.player1.score >= this.gameState.maxScore) {
-            this.gameState.winner = this.player1;
-            this.gameState.isGameOver = true;
+        if (this.player1.score >= this.maxScore) {
+            this.winner = this.player1;
+            this.isGameOver = true;
             this.displayWinMessage();
-        } else if (this.player2.score >= this.gameState.maxScore) {
-            this.gameState.winner = this.player2;
-            this.gameState.isGameOver = true;
+        } else if (this.player2.score >= this.maxScore) {
+            this.winner = this.player2;
+            this.isGameOver = true;
             this.displayWinMessage();
         }
     }
 
     resetGameState() {
-        this.ball.reset();
-        this.playerPaddle.reset();
-        this.opponentPaddle.reset();
+        this.renderManager.resetObjects();
     }
 
     updateScoreUI() {
@@ -105,6 +96,6 @@ export default class Game {
     }
 
     displayWinMessage() {
-        alert(`Game Over! ${this.gameState.winner.username} wins with a score of ${this.gameState.winner.score}!`);
+        alert(`Game Over! ${this.winner.username} wins with a score of ${this.winner.score}!`);
     }
 }
