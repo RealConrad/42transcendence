3d4c08c3043feb3e99234981e2b04ebc63715677
cwenz <wenzconrad@gmail.com>
2024-11-15 05:44:22 +0100

    Moved core game loop elements into separate class

--- frontend/scripts/game/game.js
+++ frontend/scripts/game/game.js
@@ -1,48 +1,110 @@
-import Paddle from "./Paddle.js";
-import Player from "./Player.js";
-import Ball from "./Ball.js";
-import GameState from "./GameState.js";
-import HumanController from "./HumanController.js";
-import {PADDLE_HEIGHT, PADDLE_SPEED, PADDLE_WIDTH} from "../utils/constants.js";
+import { CANVAS_WIDTH, CANVAS_HEIGHT, MAX_SCORE } from "../utils/constants.js";
 
+export default class Game {
+    constructor(canvas, ctx, player1, player2, playerPaddle, opponentPaddle, ball, playerController, opponentController) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.playerPaddle = playerPaddle;
+        this.opponentPaddle = opponentPaddle;
+        this.ball = ball;
+        this.playerController = playerController;
+        this.opponentController = opponentController;
+        this.gameState = {
+            isGameOver: false,
+            winner: null,
+            maxScore: MAX_SCORE
+        };
+    }
 
-const canvas = document.getElementById('pongCanvas');
-const ctx = canvas.getContext('2d');
+    start() {
+        requestAnimationFrame(this.gameLoop.bind(this));
+    }
 
-const player1 = new Player('Player 1');
-const player2 = new Player('Player 2');
-const playerPaddle = new Paddle(10, canvas.height / 2, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_SPEED)
-const opponentPaddle = new Paddle(canvas.width - 10, canvas.height / 2, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_SPEED)
-player1.paddle = playerPaddle;
-player2.paddle = opponentPaddle;
+    gameLoop() {
+        if (this.gameState.isGameOver) return;
 
-const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 2, 2);
+        this.update();
+        this.render();
+        requestAnimationFrame(this.gameLoop.bind(this));
+    }
 
-const gameState = new GameState(player1, player2, ball);
+    update() {
+        this.playerController.update();
+        this.opponentController.update();
+        this.ball.move();
+        this.checkCollisions();
+        this.checkScoring();
+        this.checkWinCondition();
+    }
 
-const playerController = new HumanController(playerPaddle, 'w', 's');
+    render() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.playerPaddle.draw(this.ctx);
+        this.opponentPaddle.draw(this.ctx);
+        this.ball.draw(this.ctx);
+        this.updateScoreUI();
+    }
 
-// TODO: Determine if its human or AI as opponent
-const opponentController = new HumanController(opponentPaddle, 'ArrowUp', 'ArrowDown');
+    checkCollisions() {
+        this.checkWallCollision();
+        this.checkPaddleCollision(this.playerPaddle);
+        this.checkPaddleCollision(this.opponentPaddle);
+    }
 
-const gameLoop = () => {
-    update();
-    render();
-    requestAnimationFrame(gameLoop);
+    checkWallCollision() {
+        if (this.ball.y - this.ball.radius < 0 || this.ball.y + this.ball.radius > CANVAS_HEIGHT) {
+            this.ball.speedY *= -1;
+        }
+    }
+
+    checkPaddleCollision(paddle) {
+        if (
+            this.ball.x - this.ball.radius <= paddle.x + paddle.width &&
+            this.ball.x + this.ball.radius > paddle.x &&
+            this.ball.y - this.ball.radius < paddle.y + paddle.height &&
+            this.ball.y + this.ball.radius > paddle.y
+        ) {
+            this.ball.speedX *= -1;
+        }
+    }
+
+    checkScoring() {
+        if (this.ball.x - this.ball.radius < 0) {
+            this.player2.incrementScore();
+            this.resetGameState();
+        }
+        else if (this.ball.x + this.ball.radius > CANVAS_WIDTH) {
+            this.player1.incrementScore();
+            this.resetGameState();
+        }
+    }
+
+    checkWinCondition() {
+        if (this.player1.score >= this.gameState.maxScore) {
+            this.gameState.winner = this.player1;
+            this.gameState.isGameOver = true;
+            this.displayWinMessage();
+        } else if (this.player2.score >= this.gameState.maxScore) {
+            this.gameState.winner = this.player2;
+            this.gameState.isGameOver = true;
+            this.displayWinMessage();
+        }
+    }
+
+    resetGameState() {
+        this.ball.reset();
+        this.playerPaddle.reset();
+        this.opponentPaddle.reset();
+    }
+
+    updateScoreUI() {
+        document.getElementById("player1Score").textContent = this.player1.score;
+        document.getElementById("player2Score").textContent = this.player2.score;
+    }
+
+    displayWinMessage() {
+        alert(`Game Over! ${this.gameState.winner.username} wins with a score of ${this.gameState.winner.score}!`);
+    }
 }
-
-const update = () => {
-    playerController.update();
-    opponentController.update();
-    ball.move();
-
-}
-
-const render = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    player1.paddle.draw(ctx);
-    player2.paddle.draw(ctx);
-    ball.draw(ctx);
-}
-
-gameLoop();
