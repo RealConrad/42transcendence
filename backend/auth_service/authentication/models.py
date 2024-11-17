b56b1d1548a5e13b740ce4bba8d3b737d0c511b6
cwenz <wenzconrad@gmail.com>
2024-11-17 16:51:27 +0100

    Added flag for 2fa on User model

--- backend/auth_service/authentication/models.py
+++ backend/auth_service/authentication/models.py
@@ -1,3 +1,20 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    mfa_enabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
