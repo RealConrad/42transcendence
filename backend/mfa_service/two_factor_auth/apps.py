7c5e5ce06a440e33b98c7264141f762fc7445e14
Harsh Kumbhani <Harshwut@icloud.com>
2024-11-18 20:52:03 +0100

    app for 2fa backend

---
+++ backend/mfa_service/two_factor_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TwoFactorAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'two_factor_auth'
---
+++ backend/mfa_service/two_factor_auth/models.py
@@ -0,0 +1,13 @@
+from enum import unique
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp_secret = models.CharField(max_length=16, blank=True, unique=True)
+
+
+
