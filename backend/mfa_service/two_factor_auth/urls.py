2e63cd0726f03c2d2dc88c5f18f05216f6b5471d
Harsh <Harshwut@icloud.com>
2024-11-21 15:47:19 +0100

    updated routepattern for newly added api View class

--- backend/mfa_service/two_factor_auth/urls.py
+++ backend/mfa_service/two_factor_auth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Verify2FAView
+from .views import Verify2FAView, Enable2FAView, Disable2FAView
 
 urlpatterns = [
-    path('enable/', views.enable_2fa, name='enable'),
+    path('enable/', Enable2FAView.as_view(), name='enable'),
     path('verify/', Verify2FAView.as_view(), name='verify'),
-    path('disable/', views.disable_2fa, name='disable'),
+    path('disable/', Disable2FAView.as_view(), name='disable'),
 ]
