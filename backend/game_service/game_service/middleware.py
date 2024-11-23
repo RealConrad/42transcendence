aba738a06eb7848da8c41be9f6a59338d1728e3d
cwenz <wenzconrad@gmail.com>
2024-11-23 22:00:28 +0100

    removed bug finding logs and comments, not needed anymore

--- backend/game_service/game_service/middleware.py
+++ backend/game_service/game_service/middleware.py
@@ -3,29 +3,24 @@ class SetCookieMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        print("__call__ was called")
         response = self.get_response(request)
         return self.process_response(request, response)
 
     def process_response(self, request, response):
-        # Check if a new access token was set
-        print("Checking if new cookies were set in middleware")
         if hasattr(request, 'new_access_token'):
-            print("Setting new access token in cookies")
             response.set_cookie(
                 key='access_token',
                 value=request.new_access_token,
                 httponly=True,
-                secure=False,  # Set to True in production
+                secure=False,
                 samesite='Lax',
             )
         if hasattr(request, 'new_refresh_token'):
-            print("Setting new refresh token in cookies")
             response.set_cookie(
                 key='refresh_token',
                 value=request.new_refresh_token,
                 httponly=True,
-                secure=False,  # Set to True in production
+                secure=False,
                 samesite='Lax',
             )
         return response
