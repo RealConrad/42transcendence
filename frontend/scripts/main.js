796f2a87d07dfed9106a713056709b38028e4263
cwenz <wenzconrad@gmail.com>
2024-11-24 01:55:00 +0100

    added api wrapper function for frontend

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,3 +1,5 @@
+let accessToken;
+
 const refreshTokens = async () => {
     await fetch('http://127.0.0.1:8002/api/token/refresh/', {
             method: "POST",
@@ -7,19 +9,19 @@ const refreshTokens = async () => {
                return response.json();
             } else {
                 // TODO: logout user
-                console.error("Unable to refresh tokens. loggingo ut")
+                console.error("Unable to refresh tokens. logging out")
             }
     }).then((data) => {
-        window.accessToken = data.access_token;
+        accessToken = data.access_token;
         console.log("Tokens refreshed");
     }).catch((error) => {
         console.error("Unable to refresh tokens: ", error);
-    })
+    });
 }
 
 window.onload = async () => {
     console.log("Page refreshed, trying to get new tokens....");
-    if (!window.accessToken) {
+    if (!accessToken) {
         try {
             await refreshTokens();
         } catch (error) {
@@ -28,4 +30,35 @@ window.onload = async () => {
     } else {
         console.log("Already have access token");
     }
+}
+
+/**
+ * Handles API calls the server. The caller should convert to json and handle appropriately
+ * @param url The URL of the API
+ * @param options Any additional headers/payload can be added here
+ * @returns {Promise<Response>} The response from the server
+ */
+export const apiCall = async (url, options = {}) => {
+    if (!accessToken) {
+        await refreshTokens();
+    }
+
+    options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${accessToken}`,
+    };
+    const response = await fetch(url, options);
+
+    if (response.status === 401) {
+        console.warn("Access token expired, refreshing...");
+        await refreshTokens()
+        options.headers.Authorization = `Bearer ${accessToken}`;
+        return fetch(url, options);
+    }
+    if (!response.ok) {
+        const error = await response.json();
+        console.error(`API Error: ${response.status}`, error);
+        throw new Error(error.error || "API call failed");
+    }
+    return response
 }
