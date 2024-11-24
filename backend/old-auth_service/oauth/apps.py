8884612a18e0f797d040e64a4db344f9e67f1fa6
cwenz <wenzconrad@gmail.com>
2024-11-24 02:56:05 +0100

    Deleted the old auth service

--- backend/old-auth_service/oauth/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class OauthConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = 'oauth'
-
-	def ready(self):
-		import oauth.signals
