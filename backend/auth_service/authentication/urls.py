afe2665d42668c8cb13ad32e41d94fdd5ca44876
Harsh Kumbhani <Harshwut@icloud.com>
2024-11-26 21:00:30 +0100

    added view for changing multi-factor authentication flag

--- backend/auth_service/authentication/urls.py
+++ backend/auth_service/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, SetMFAFlagView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('setmfaflag/', SetMFAFlagView.as_view(), name='setmfaflag')
 ]
