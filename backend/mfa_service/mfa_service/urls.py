7ec767b0677c6f789325feed4b3b5c55d5b8ddd9
Harsh Kumbhani <Harshwut@icloud.com>
2024-11-27 15:48:09 +0100

    changed mfa api url

--- backend/mfa_service/mfa_service/urls.py
+++ backend/mfa_service/mfa_service/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api-2fa/', include('two_factor_auth.urls')),
+    path('api/2fa/', include('two_factor_auth.urls')),
 ]
