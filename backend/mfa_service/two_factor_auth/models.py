c1564dd05b884cd4044c114e2d795b5edbca1572
Harsh Kumbhani <Harshwut@icloud.com>
2024-11-28 22:38:10 +0100

    added 2fa enable flag in the data model

--- backend/mfa_service/two_factor_auth/models.py
+++ backend/mfa_service/two_factor_auth/models.py
@@ -7,4 +7,5 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     otp_secret = models.CharField(max_length=32, blank=True, null=True)
+    mfa_enabled = models.BooleanField(default=False)
 
