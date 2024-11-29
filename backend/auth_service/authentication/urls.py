e05c2c253f22c7466fa3ab29cb17dcb30f7b8bc7
Harsh Kumbhani <Harshwut@icloud.com>
2024-11-29 17:24:54 +0100

    url to get user data

--- backend/auth_service/authentication/urls.py
+++ backend/auth_service/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, SetMFAFlagView, SaveProfilePicture
+from .views import RegisterView, LoginView, SetMFAFlagView, SaveProfilePicture, GetUserData
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('setmfaflag/', SetMFAFlagView.as_view(), name='setmfaflag'),
     path('save_profile_pic/', SaveProfilePicture.as_view(), name='save_profile_pic'),
+    path('get_user_data/', GetUserData.as_view(), name='get_user_data')
 ]
