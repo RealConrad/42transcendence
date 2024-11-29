c0da8520182f1afa99ba9566b23914fdd29f4f3b
Harsh Kumbhani <Harshwut@icloud.com>
2024-11-29 12:08:02 +0100

    added api url to save the profile picture

--- backend/auth_service/authentication/urls.py
+++ backend/auth_service/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView, SetMFAFlagView
+from .views import RegisterView, LoginView, SetMFAFlagView, SaveProfilePicture
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('setmfaflag/', SetMFAFlagView.as_view(), name='setmfaflag')
+    path('setmfaflag/', SetMFAFlagView.as_view(), name='setmfaflag'),
+    path('save_profile_pic/', SaveProfilePicture.as_view(), name='save_profile_pic'),
 ]
