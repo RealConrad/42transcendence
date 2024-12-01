ffd521844a97194a21d41f37f9f85716b632676d
Harsh Kumbhani <Harshwut@icloud.com>
2024-12-01 11:33:08 +0100

    data field to store image url for 42 OAuth

--- backend/auth_service/authentication/models.py
+++ backend/auth_service/authentication/models.py
@@ -6,6 +6,7 @@ class CustomUser(AbstractUser):
     total_matches_won = models.IntegerField(default=0)
     total_matches_lost = models.IntegerField(default=0)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    profile_picture_url = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.username
