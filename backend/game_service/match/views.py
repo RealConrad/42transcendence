ea75fb1eacd70f466e38f2e7a1d8bc765dcd4ab4
Harsh Kumbhani <Harshwut@icloud.com>
2024-12-04 20:51:27 +0100

    added logic to update tournaments

--- backend/game_service/match/views.py
+++ backend/game_service/match/views.py
@@ -1,13 +1,16 @@
 from multiprocessing.context import AuthenticationError
 from rest_framework import serializers
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import BasePermission
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from match.models import Match
-from match.serializers import MatchSerializer
+from .models import Match, UserProfile
+from .serializers import UserProfileSerializer, MatchSerializer
 import logging
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +28,28 @@ class SaveMatchView(CreateAPIView):
         except Exception as e:
             logger.error(f"Unexpected Error: {e}")
             raise
+
+class UpdateTournamentStatusView(APIView):
+    """
+    Updates tournament stats for the user
+    """
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        action = request.data.get('action')
+        profile = get_object_or_404(UserProfile, user=request.user)
+
+        if action == 'played':
+            profile.increment_tournaments_played()
+        elif action == 'won':
+            profile.increment_tournaments_won()
+            profile.increment_tournaments_played()
+        else:
+            return Response({"detail": "Invalid action"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = UserProfileSerializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class MatchHistoryView(ListAPIView):
     permission_classes = [IsAuthenticated]
