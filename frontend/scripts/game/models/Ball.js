fae861497dbd7aa2b7f2dd50f66b2ada4727c8b1
cwenz <wenzconrad@gmail.com>
2024-12-07 23:11:16 +0100

    improved initial ball direction when game starts

--- frontend/scripts/game/models/Ball.js
+++ frontend/scripts/game/models/Ball.js
@@ -1,17 +1,34 @@
 import Renderable from "./Renderable.js";
 
 export default class Ball extends Renderable {
-    constructor(x, y, radius, speedX, speedY) {
+    constructor(x, y, radius) {
         super();
         this.x = x;
         this.y = y;
         this.initialX = x;
         this.initialY = y;
         this.radius = radius;
-        this.speedX = speedX;
-        this.speedY = speedY;
+
+        const angleDegrees = this.getRandomAngle();
+        const angleRadians = angleDegrees * (Math.PI / 180);
+
+        const speedMagnitude = 7;
+        this.speedX = speedMagnitude * Math.cos(angleRadians);
+        this.speedY = speedMagnitude * Math.sin(angleRadians);
+
         this.lastTouchedPlayer = null;
         this.trail = [];
+    }
+
+   getRandomAngle() {
+        const intervals = [
+            { start: 0, end: 45 },    // 0° to 60°
+            { start: 135, end: 180 }, // 120° to 240°
+            { start: 180, end: 225 }, // 120° to 240°
+            { start: -45, end: 0 }    // -60° to 0°
+        ];
+        const chosenInterval = intervals[Math.floor(Math.random() * intervals.length)];
+        return chosenInterval.start + Math.random() * (chosenInterval.end - chosenInterval.start);;
     }
 
     move() {
