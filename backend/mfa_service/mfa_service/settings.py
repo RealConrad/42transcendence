1882d6e076e21cd487517199930c929bfebb5c42
Harsh Kumbhani <Harshwut@icloud.com>
2024-12-07 20:50:40 +0100

    removed unused secret key

--- backend/mfa_service/mfa_service/settings.py
+++ backend/mfa_service/mfa_service/settings.py
@@ -150,7 +150,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# JWT SECRET key
-
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
