a2f67ab0a66c177e0950dea88e89ba5e5b0f1284
cwenz <wenzconrad@gmail.com>
2024-12-08 07:18:23 +0100

    Added field to update display name

--- frontend/scripts/pages/components/AccountMenu.js
+++ frontend/scripts/pages/components/AccountMenu.js
@@ -6,7 +6,7 @@ import {
     getLocal2FA,
     setLocal2FA,
     disable2FA,
-    logout
+    logout, validateInput, showToast
 } from "../../api/api.js";
 import GlobalEventEmitter from "../../utils/EventEmitter.js";
 import {BASE_AUTH_API_URL, EVENT_TYPES, USER} from "../../utils/constants.js";
@@ -18,11 +18,11 @@ export class AccountMenu extends HTMLElement {
         this.attachShadow({mode: 'open'})
         this.accessToken = null;
         this.imgUrl = null;
-        this.username = null;
+        this.displayName = null;
     }
     connectedCallback() {
         this.accessToken = getAccessToken();
-        this.username = USER.username;
+        this.displayName = USER.username;
         this.imgUrl = USER.profilePicture;
         this.render();
         if (this.accessToken){
@@ -36,61 +36,70 @@ export class AccountMenu extends HTMLElement {
         this.shadowRoot.innerHTML = this.html();
     }
 
-    html() {
-        return `
-            <link rel="stylesheet" href="../../../styles/account.css">
-    
-            ${!this.accessToken ? 
-                `
-                    <div class="guest-view">
-                        Login first to view this page!
-                    </div>
-                `
-                :
-                `
-                    <div class="container">
-                        <div class="heading">
-                            <div class="profile-picture" id="profile-picture-id">
-                                <img src="${this.imgUrl}" alt=""> 
-                                <span class="initial"></span>
-                            </div>
-                            <div>
-                                ${this.username}
-                            </div>
+html() {
+    return `
+        <link rel="stylesheet" href="../../../styles/account.css">
+        <link id="style-sheet2" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        ${!this.accessToken ? 
+            `
+                <div class="guest-view">
+                    Login first to view this page!
+                </div>
+            `
+            :
+            `
+                <div class="container">
+                    <div class="heading">
+                        <div class="profile-picture" id="profile-picture-id">
+                            <img src="${this.imgUrl}" alt=""> 
+                            <span class="initial"></span>
                         </div>
-                        <div class="flex-container">
-                            <button class="orange-2FA-button" id="TwoFactorAuthButton">Enable 2FA</button>
-                            <button class="orange-2FA-button" id="logout">Logout</button>
-                        </div>
-                        <div class="total-match-stats">
-                            <div class="matches-won">
-                                MATCHES 
-                                <div>WON</div>
-                                <div class="score" id="matches-won">0</div>
-                            </div>
-                            <div class="matches-lost">
-                                MATCHES 
-                                <div>LOST</div>
-                                <div class="score" id="matches-lost">0</div>
-                            </div>
-                        </div>
-                        <div class="tournament-stats">
-                            <div class="tournaments">
-                                TOURNAMENTS
-                                <div id="tournament-stats">0 won of 0 played</div>
-                            </div>
-                        </div>
-                        <div class="previous-matches-container">
-                            <div class="previous-matches-heading">Previous Matches</div>
-                            <div class="previous-matches" id="previous-matches">
-                                <!-- Matches will be dynamically rendered here -->
+                        <div class="username-container">
+                            <span id="username-display">${this.displayName}</span>
+                            <input id="username-input" type="text" value="${this.displayName}" style="display: none;">
+                            <svg id="edit-username-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                            </svg>
+                            <div id="username-edit-buttons" style="display: none;">
+                                <button id="save-username" class="btn btn-success btn-sm">✔</button>
+                                <button id="cancel-username" class="btn btn-danger btn-sm">✖</button>
                             </div>
                         </div>
                     </div>
-                `
-                }
-            `;
-        }
+                    <div class="flex-container">
+                        <button class="orange-2FA-button" id="TwoFactorAuthButton">Enable 2FA</button>
+                        <button class="orange-2FA-button" id="logout">Logout</button>
+                    </div>
+                    <div class="total-match-stats">
+                        <div class="matches-won">
+                            MATCHES 
+                            <div>WON</div>
+                            <div class="score" id="matches-won">0</div>
+                        </div>
+                        <div class="matches-lost">
+                            MATCHES 
+                            <div>LOST</div>
+                            <div class="score" id="matches-lost">0</div>
+                        </div>
+                    </div>
+                    <div class="tournament-stats">
+                        <div class="tournaments">
+                            TOURNAMENTS
+                            <div id="tournament-stats">0 won of 0 played</div>
+                        </div>
+                    </div>
+                    <div class="previous-matches-container">
+                        <div class="previous-matches-heading">Previous Matches</div>
+                        <div class="previous-matches" id="previous-matches">
+                            <!-- Matches will be dynamically rendered here -->
+                        </div>
+                    </div>
+                </div>
+            `
+            }
+        `;
+}
+
 
     toggle2faButton(status){
         const TwoFactorAuthButton = this.shadowRoot.getElementById("TwoFactorAuthButton");
@@ -142,6 +151,67 @@ export class AccountMenu extends HTMLElement {
                 })
             }
         });
+
+        // Edit username elements
+        const editIcon = this.shadowRoot.getElementById("edit-username-icon");
+        const usernameDisplay = this.shadowRoot.getElementById("username-display");
+        const usernameInput = this.shadowRoot.getElementById("username-input");
+        const editButtons = this.shadowRoot.getElementById("username-edit-buttons");
+        const saveButton = this.shadowRoot.getElementById("save-username");
+        const cancelButton = this.shadowRoot.getElementById("cancel-username");
+
+        // Edit username logic
+        editIcon.addEventListener("click", () => {
+            usernameDisplay.style.display = "none";
+            usernameInput.style.display = "block";
+            editButtons.style.display = "inline-flex";
+            editIcon.style.display = "none";
+        });
+
+        saveButton.addEventListener("click", async () => {
+            const newDisplayName = usernameInput.value.trim();
+            if (newDisplayName && newDisplayName !== this.username) {
+                try {
+                    validateInput(newDisplayName);
+                    console.log(newDisplayName);
+                    const response = await apiCall(`${BASE_AUTH_API_URL}/update_username/`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ displayName: newDisplayName }),
+                    });
+
+                    if (response.ok) {
+                        this.username = newDisplayName;
+                        USER.username = newDisplayName;
+                        usernameDisplay.textContent = newDisplayName;
+                        GlobalEventEmitter.emit(EVENT_TYPES.RELOAD_DASHBOARD, {});
+                    } else {
+                        showToast('Failed to update display name, try again later', 'danger');
+                        console.error("Failed to update username");
+                    }
+                } catch (error) {
+                    showToast(error, 'danger');
+                    console.error("Error updating username:", error);
+                }
+            }
+            // Revert to display mode
+            usernameDisplay.style.display = "block";
+            usernameInput.style.display = "none";
+            editButtons.style.display = "none";
+            editIcon.style.display = "inline";
+        });
+
+        cancelButton.addEventListener("click", () => {
+            // Revert to original username
+            usernameInput.value = this.username;
+            usernameDisplay.style.display = "block";
+            usernameInput.style.display = "none";
+            editButtons.style.display = "none";
+            editIcon.style.display = "inline";
+        });
+
     }
 
     async renderPreviousMatches() {
