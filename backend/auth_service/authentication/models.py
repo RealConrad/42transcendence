5feb1cb6ae89277fc99df222b98ca49c4abe1c33
Harsh Kumbhani <Harshwut@icloud.com>
2024-12-08 11:32:13 +0100

    added display name data field, defaults to username

--- backend/auth_service/authentication/models.py
+++ backend/auth_service/authentication/models.py
@@ -6,6 +6,12 @@ class CustomUser(AbstractUser):
     logged_in = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
     profile_picture_url = models.URLField(max_length=500, null=True, blank=True)
+    display_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.display_name:
+            self.display_name = self.username
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
