f82724f00b1d215c6d8e9a0de859fbbeb2df48e4
Harsh Kumbhani <Harshwut@icloud.com>
2024-12-08 08:25:33 +0100

    api to check if user exists in the database or not

--- backend/auth_service/authentication/urls.py
+++ backend/auth_service/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, SetMFAFlagView, SaveProfilePicture, GetUserData
+from .views import RegisterView, LoginView, SetMFAFlagView, SaveProfilePicture, GetUserData, CheckUserExistence
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('setmfaflag/', SetMFAFlagView.as_view(), name='setmfaflag'),
     path('save_profile_pic/', SaveProfilePicture.as_view(), name='save_profile_pic'),
-    path('get_user_data/', GetUserData.as_view(), name='get_user_data')
+    path('get_user_data/', GetUserData.as_view(), name='get_user_data'),
+    path('check_user_existence/', CheckUserExistence.as_view(), name='check_user_existence'),
 ]
